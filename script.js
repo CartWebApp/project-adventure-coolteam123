c86f636e7226e0b343f8adcfb858afaf93dd6c31
Matthew <73604168+QWERTYUIOPDADERP@users.noreply.github.com>
2025-03-28 15:44:01 -0700

    started the game! just some button / layout set up to get an idea of how it should go. A little js too, for the same reason.

---
+++ script.js
@@ -0,0 +1,19 @@
+const text = document.getElementById('text');
+
+const story = new Object();
+story.text = [
+    `Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.`,
+    `Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.`
+];
+story.textNum = 0;
+
+function startGame(){
+    text.innerText = story.text[story.textNum];
+}
+
+function nextText(){
+    if(story.textNum < (story.text.length-1)){
+        story.textNum++;
+        text.innerText = story.text[story.textNum];
+    }
+}
